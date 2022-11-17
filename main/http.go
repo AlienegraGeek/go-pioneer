e37fc30f45cc0eb1f47da7fa240281392200e60b
AlienegraGeek <969024145@qq.com>
2022-11-17 21:36:03 +0800

    mage random str test

--- main/http.go
+++ main/http.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"time"
 )
+
+var CHARS = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
 
 func main() {
 
@@ -13,9 +20,10 @@ func main() {
 	http.HandleFunc("/post", postHandler)
 	http.HandleFunc("/web/login", loginHandler)
 	http.HandleFunc("/zee/test", zeeTestHandler)
+	http.HandleFunc("/mage/test", mageTestHandler)
 	// addr：监听的地址
 	// handler：回调函数
-	http.ListenAndServe("192.168.2.142:9000", nil)
+	http.ListenAndServe("192.168.1.32:9000", nil)
 }
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
@@ -90,4 +98,41 @@ func zeeTestHandler(w http.ResponseWriter, r *http.Request) {
 	//    "msg": "操作成功"
 	//}`
 	w.Write([]byte(answer))
+}
+
+type Res struct {
+	Code string `json:"code"`
+	Res  string `json:"res"`
+}
+
+func mageTestHandler(w http.ResponseWriter, r *http.Request) {
+	randStr := GetRoundName(1)
+	defer r.Body.Close()
+	//1. 请求类型是aplication/x-www-form-urlencode时解析form数据
+	fmt.Println(r.Body)
+	//r.ParseForm()
+	//fmt.Println(r.PostForm) //打印form数数据
+	//fmt.Println(r.PostForm.Get("deviceSerial"))
+	//2. 请求类型是application/json时从r.Body读取数据
+	b, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("read request.Body failed, err", err)
+		return
+	}
+	fmt.Println(string(b))
+	//answer := `{"data":{"code":"0","msg":"success"}}`
+	//answer, _ := json.Marshal(rs)
+	answer, _ := json.Marshal(Res{Code: "0", Res: randStr})
+	w.Write(answer)
+}
+
+func GetRoundName(size int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < size; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
 }
